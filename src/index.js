725012109343fa683b375e1f79e048e5a89cbca3
buwenzheng <>
2018-02-08 16:04:35 +0800

    study flux

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 // import App from './App';
-import ControlPanel from './2-props/ControlPanel.js'
+import ControlPanel from './3-flux/views/ControlPanel.js'
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<ControlPanel />, document.getElementById('root'));
